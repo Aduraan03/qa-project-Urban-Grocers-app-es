2819113535e754504c325edbd7a126200636a9da
Alejandro <adhm_03@hotmail.com>
2024-11-12 17:08:18 -0600

    Updated according the following review: Por revisar:
    El archivo README.md debe contener más detalle sobre: Acerca del proyecto, Qué tecnologías usaste, Detalles de cómo instalar y usar las librerías, Cómo puedo ejecutar tus pruebas en la terminal, Tu nombre y sprint del proyecto
    Recuerda que para ejecutar con PyTest, se utiliza el comando pytest folder/del/proyecto en la terminal
    En todas tus pruebas, no hay que hard-codear los datos, especialmente datos de prueba :)! Hay que extraer todos los datos al archivo data.py para que lo utilices de la forma: get_kit_body(data.one_letter) en lugar de get_kit_body("a"). Hay que hacerlo para todas las pruebas
    En tu función get_kit_body en el archivo main.py debes mandar el campo "name", no "Content-Type"
    Este código:
    
    def get_kit_body(name):
        return {
            "Content-Type": "application/json"
        }
    Debería ser:
    
    def get_kit_body(name):
        return {
            "name": name
        }
    Tu test #8 debería mandar el kit_body sin el campo name
    Para esto puede usar la función corregida (Ver el cómentario anterior) y usar None:
    kit_body = get_kit_body(data.no_name)
    En donde en tu archivo data.py tendrás no_name = None
    En tu función post_new_kits() trata de utilizar los headers desde el data.py pero sin poner el authorization... Así los puedes reutilizar también en tus otras peticiones que ya tienes en lugar de hard.codearlos! Hay que insertar el Authorization dentro de una copia del data.headers
    Un ejemplo sería cambiar este código:
    
    def post_new_client_kit(kit_body):
        headers = {
            "Content-Type": "application/json"
        }
        response = requests.post(f"{URL_SERVICE}{KITS_PATH}", json=kit_body, headers=headers)
        return response
    Por este código:
    
    def post_new_kits(new_kit):
    def post_new_client_kit(kit_body):
        token = get_new_user_token()
        headers = data.headers.copy()
        headers["Authorization"] = f"Bearer {token}"
        response = requests.post(f"{URL_SERVICE}{KITS_PATH}", json=kit_body, headers=headers)
        return response
    Y en tu archivo data.py tendrás:
    
    headers = {
        "Content-Type": "application/json"
    }
    Para la función de crear el usuario, reutiliza los headers también:
    
    def get_new_user_token():
        url = f"{URL_SERVICE}{CREATE_USER_PATH}"
        headers = data.headers.copy()
        user_body = get_user_body()  # Asegúrate de que este cuerpo esté bien definido en data.py
    
        response = requests.post(url, headers=headers, json=user_body)
        response.raise_for_status()  # Verifica que la solicitud sea exitosa
        return response.json()["authToken"]  # Obtén el authToken si la respuesta es exitosa

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -1,54 +1,55 @@
 import pytest
-from sender_stand_request import create_kit, get_kit, update_kit, delete_kit, post_new_client_kit
-from data import get_kit_body, kit_data, updated_kit_data
+from sender_stand_request import  post_new_client_kit
+import data
 
 
 
 # Pruebas positivas
 def test_kit_name_1_char():
-    response = post_new_client_kit(get_kit_body("a"))
+    response = post_new_client_kit(data.get_kit_body(data.one_letter))
     assert response.status_code == 201
-    assert response.json()["name"] == "a"
+    assert response.json()["name"] == data.one_letter
 
 def test_kit_name_511_chars():
     name = "a" * 511
-    response = post_new_client_kit(get_kit_body(name))
+    response = post_new_client_kit(data.get_kit_body(name))
     assert response.status_code == 201
     assert response.json()["name"] == name
 
 def test_kit_name_special_chars():
     name = "№%@"
-    response = post_new_client_kit(get_kit_body(name))
+    response = post_new_client_kit(data.get_kit_body(data.special_chars))
     assert response.status_code == 201
-    assert response.json()["name"] == name
+    assert response.json()["name"] == data.special_chars
 
 def test_kit_name_with_spaces():
     name = " A Aaa "
-    response = post_new_client_kit(get_kit_body(name))
+    response = post_new_client_kit(data.get_kit_body(data.name_with_spaces))
     assert response.status_code == 201
-    assert response.json()["name"] == name
+    assert response.json()["name"] == data.name_with_spaces
 
 def test_kit_name_with_numbers():
     name = "123"
-    response = post_new_client_kit(get_kit_body(name))
+    response = post_new_client_kit(data.get_kit_body(data.name_with_numbers))
     assert response.status_code == 201
-    assert response.json()["name"] == name
+    assert response.json()["name"] == data.name_with_numbers
 
 # Pruebas negativas
 def test_kit_name_empty():
-    response = post_new_client_kit(get_kit_body(""))
+    response = post_new_client_kit(data.get_kit_body(data.empty_name))
     assert response.status_code == 400
 
 def test_kit_name_exceeds_512_chars():
     name = "a" * 512
-    response = post_new_client_kit(get_kit_body(name))
+    response = post_new_client_kit(data.get_kit_body(name))
     assert response.status_code == 400
 
 def test_kit_name_not_passed():
-    response = post_new_client_kit({})
+    kit_body = data.get_kit_body(data.no_name)
+    response = post_new_client_kit(kit_body)
     assert response.status_code == 400
 
 def test_kit_name_different_type():
-    response = post_new_client_kit(get_kit_body(123))
+    response = post_new_client_kit(data.get_kit_body(data.non_string_name))
     assert response.status_code == 400
 
